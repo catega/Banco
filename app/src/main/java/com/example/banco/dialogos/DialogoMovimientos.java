e04b73903976b441d57efc08e4f1584b096b395f
Carlos <cartegar@gmail.cim>
2020-11-20 13:54:01 +0100

    Dialogo Movimientos

---
+++ app/src/main/java/com/example/banco/dialogos/DialogoMovimientos.java
@@ -0,0 +1,30 @@
+package com.example.banco.dialogos;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.ArrayList;
+
+public class DialogoMovimientos extends DialogFragment {
+    public String info;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Información detallada").setMessage(info).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        return builder.create();
+    }
+}
