0f3a96f6bcc520df58676189814ac1cd98fbe7a1
Carlos Teruel <cartegar@gmail.com>
2020-11-24 18:13:47 +0100

    Dialogo movimientos en custom layout.

--- app/src/main/java/com/example/banco/dialogos/DialogoMovimientos.java
+++ app/src/main/java/com/example/banco/dialogos/DialogoMovimientos.java
@@ -4,10 +4,15 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.example.banco.R;
 
 import java.util.ArrayList;
 
@@ -17,8 +22,12 @@ public class DialogoMovimientos extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialogo_movimientos, null);
+        TextView text = (TextView) view.findViewById(R.id.txtDialogoMovimientos);
+        text.setText(info);
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Información detallada").setMessage(info).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+        builder.setTitle("Información detallada").setView(view).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.cancel();
